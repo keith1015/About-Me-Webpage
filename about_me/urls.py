5143ac017633c5b5ba59ae90606f7fb97f709bea
keith1015 <chungagk@gmail.com>
2017-11-05 23:12:05 -0500

    First commit

---
+++ about_me/urls.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import url
+
+from .views import Home_page_view
+
+urlpatterns = [
+    url(r'^$', Home_page_view.as_view()),
+]
---
+++ about_me/views.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class Home_page_view(TemplateView):
+    template_name = 'about_me/index.html'
